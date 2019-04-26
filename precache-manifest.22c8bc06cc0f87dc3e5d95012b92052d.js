d9071fb61d19ba7ef05440beb659b1827b0d5a1d
kianga722 <kianga722@gmail.com>
2019-04-25 21:07:55 -0400

    Updates

---
+++ precache-manifest.22c8bc06cc0f87dc3e5d95012b92052d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3eda30c390abe729dfa00f00f4138403",
+    "url": "/portfolio/static/media/motivator.3eda30c3.png"
+  },
+  {
+    "revision": "0a0d062de3ff0a6de38a",
+    "url": "/portfolio/static/css/main.d9e8e08c.chunk.css"
+  },
+  {
+    "revision": "f32a99e386b27f091e15",
+    "url": "/portfolio/static/js/1.f32a99e3.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "93e9ce5955069738cb43df11284bee03",
+    "url": "/portfolio/static/media/profPic.93e9ce59.png"
+  },
+  {
+    "revision": "88e4e5aa079ff87ea138e268849576f6",
+    "url": "/portfolio/static/media/layoutscroll.88e4e5aa.png"
+  },
+  {
+    "revision": "0a0d062de3ff0a6de38a",
+    "url": "/portfolio/static/js/main.0a0d062d.chunk.js"
+  },
+  {
+    "revision": "9205d183dbd95724fbf86f626adaae0d",
+    "url": "/portfolio/static/media/ytshuffle.9205d183.png"
+  },
+  {
+    "revision": "861995116623fe9bdf616fe0ae391696",
+    "url": "/portfolio/static/media/cnotify.86199511.jpg"
+  },
+  {
+    "revision": "7cf389a4471a8929214a1e5698a44e35",
+    "url": "/portfolio/static/media/statusw.7cf389a4.png"
+  },
+  {
+    "revision": "506d024d857ff3b57806b0e88dab2888",
+    "url": "/portfolio/static/media/yourinfo.506d024d.png"
+  },
+  {
+    "revision": "e3b2ad192d5e86a90a2af1b09891f907",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3b02543a44555b2e54dbcfb2cff2a44b.js"
+  "/portfolio/precache-manifest.22c8bc06cc0f87dc3e5d95012b92052d.js"
 );
 
 workbox.clientsClaim();
