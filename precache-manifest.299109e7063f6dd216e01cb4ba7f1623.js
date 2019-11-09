8de088971356d7fb4cd68d8b4ff4f0a18168be26
kianga722 <kianga722@gmail.com>
2019-11-08 22:40:49 -0500

    Updates

---
+++ precache-manifest.299109e7063f6dd216e01cb4ba7f1623.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cc9673f3d9eb7088d3cbdb8ab94b9dc",
+    "url": "/static/media/motivator.0cc9673f.png"
+  },
+  {
+    "revision": "01dc73f173a44016ce1d",
+    "url": "/static/css/main.675e2c16.chunk.css"
+  },
+  {
+    "revision": "1514e063a234d836c3b0",
+    "url": "/static/js/1.1514e063.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8b98aafdbb9d1ad1c222240aed3f24b",
+    "url": "/static/media/profPic.b8b98aaf.png"
+  },
+  {
+    "revision": "2861a7e71461cf06f78a7c8d61193e82",
+    "url": "/static/media/531workout.2861a7e7.png"
+  },
+  {
+    "revision": "3bdca3b759e2d0616d6cdbfe9d3437f0",
+    "url": "/static/media/layoutscroll.3bdca3b7.png"
+  },
+  {
+    "revision": "01dc73f173a44016ce1d",
+    "url": "/static/js/main.01dc73f1.chunk.js"
+  },
+  {
+    "revision": "f555ed038585bb9565a45e5961e2f93c",
+    "url": "/static/media/nsong.f555ed03.png"
+  },
+  {
+    "revision": "fc10ffaecd4debb1ad0d61a01e594826",
+    "url": "/static/media/ytshuffle.fc10ffae.png"
+  },
+  {
+    "revision": "78bec770ca1792d85324f569416e8d6d",
+    "url": "/static/media/cnotify.78bec770.png"
+  },
+  {
+    "revision": "f22dc2c32cfbe2c24246eb48f62c5945",
+    "url": "/static/media/statusw.f22dc2c3.png"
+  },
+  {
+    "revision": "e4082132d1a28b4e76f09963c7e0f65d",
+    "url": "/static/media/yourinfo.e4082132.png"
+  },
+  {
+    "revision": "480c9431548ca67fb642fbf070c633ed",
+    "url": "/index.html"
+  }
+];
